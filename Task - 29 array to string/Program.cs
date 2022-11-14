f4f873bde7ca37b686136d7b61e6983339b4a439
Nelli <nellichka0801@yandex.ru>
2022-11-14 15:18:40 +1200

    Array in a digit

---
+++ Task - 29 array to string/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая задаёт массив из 8 целых 
+//чисел с клавиатуры и далее выводит массив на экран в одну строку
+
+void ArrayFromInput()
+    {
+        int[] arr = new int[8];
+        for (int i = 0; i < 8; i++) {
+            Console.WriteLine("Введите число массива и нажмите клавишу enter");
+            arr[i] = int.Parse(Console.ReadLine()!);
+        }
+        string s = "";
+        for(int i = 0; i < arr.Length; i++)
+        {
+            s += arr[i];
+        }
+        Console.WriteLine("Массив "+ s);
+        Console.WriteLine("");
+    }
+    
+ArrayFromInput();
+
