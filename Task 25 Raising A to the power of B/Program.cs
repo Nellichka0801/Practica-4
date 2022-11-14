74d2d84ab963a74b46aa8030d0504bdc3b4bdfea
Nelli <nellichka0801@yandex.ru>
2022-11-14 12:26:56 +1200

    Raising A to the power of B

---
+++ Task 25 Raising A to the power of B/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+ int myPow(int a, int b)
+    {
+        int number = a;
+        for(int i = 0; i < b-1; i++)
+        {
+            number *= a;
+        }
+        return number;
+    }
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите степень");
+int degree = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("результат: "+ myPow(number,degree));
+
+
